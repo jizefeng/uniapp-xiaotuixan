7dd397337af7712e0651344ee9d22f0ba7484ab7
jizefeng <205402866@qq.com>
2025-04-23 10:23:57 +0800

    分类面板组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,7 @@ import { getHomeBannerApi } from '@/services/home.ts'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/home'
+import CategoryPanel from '@/pages/index/componets/CategoryPanel.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -19,11 +20,17 @@ onLoad(() => {
 </script>
 
 <template>
+  <!--  自定义导航栏-->
   <CustomNavbar />
+  <!--  自定义轮播图-->
   <XtxSwiper :list="bannerList" />
+  <!--  分类面板-->
+  <CategoryPanel />
   <view class="index"></view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
