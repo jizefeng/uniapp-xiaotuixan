08927f46149bf171e47db24a9c7caeae3bc7f44d
jizefeng <205402866@qq.com>
2025-05-03 23:52:40 +0800

    订单模块-基础搭建

---
+++ src/services/order.ts
@@ -0,0 +1,11 @@
+import { http } from '@/utils/http.ts'
+import type { OrderPreResult } from '@/types/order'
+/**
+ * 填写订单-获取预付订单
+ */
+export const getMemberOrderPreAPI = () => {
+  return http<OrderPreResult>({
+    method: 'GET',
+    url: '/member/order/pre',
+  })
+}
