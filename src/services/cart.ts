61a33f5f62db2f2168d13782420516dc75878732
jizefeng <205402866@qq.com>
2025-05-03 15:58:46 +0800

    购物车模块-列表渲染

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,4 +1,5 @@
 import { http } from '@/utils/http.ts'
+import type { CartItem } from '@/types/cart'
 
 /**
  *  添加购物车
@@ -9,5 +10,14 @@ export const postMemberCartAPI = (data: { skuId: string; count: number }) => {
     method: 'POST',
     url: '/member/cart',
     data,
+  })
+}
+/**
+ * 获取购物车列表
+ */
+export const getMemberCartAPI = () => {
+  return http<CartItem[]>({
+    method: 'GET',
+    url: '/member/cart',
   })
 }
